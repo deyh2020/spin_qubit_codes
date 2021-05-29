98fe0cf2a1cacbcd5f124665d89dbc8cbd81c9e0
fabrizioberr <fabrizio.berritta@nbi.ku.dk>
2021-05-29 12:27:27 +0200

    Update plotting_S_parameters_23Apr2021.py

--- plots/1D traces/plotting_S_parameters_23Apr2021.py
+++ plots/1D traces/plotting_S_parameters_23Apr2021.py
@@ -4,7 +4,30 @@ Created on Tue Dec 15 17:47:54 2020
 
 @author: T7
 """
+# %% %matplotlib inline
+import os
+from time import sleep
 
+import matplotlib.pyplot as plt
+import numpy as np
+import qcodes as qc
+from qcodes import (
+    Measurement,
+    experiments,
+    initialise_database,
+    initialise_or_create_database_at,
+    load_by_guid,
+    load_by_run_spec,
+    load_experiment,
+    load_last_experiment,
+    load_or_create_experiment,
+    new_experiment,
+)
+from qcodes.dataset.plotting import plot_dataset, plot_by_id
+# %%
+import matplotlib.gridspec as gridspec
+# %%
+initialise_or_create_database_at("/Users/fabrizioberr/Dropbox/Triton 4/fabrizio qdev dropbox/swap/T4_UPGRADE.db"
 # %%
 fig = plt.figure()
 gs1 = gridspec.GridSpec(10,10)
@@ -20,7 +43,7 @@ axes1,colorbar1 = plot_by_id(24,axes=ax, label='Coax 6')
 ax.set_xlabel('Frequency (GHz)',size=30)
 ax.set_ylabel('S21 (dB)',size=30)
 ax.tick_params(labelsize=20) 
-ax.set_title('RT transmission test',size=30)
+ax.set_title('RT transmission test - FB 25 May 2021 - database.db',size=30)
 ax.grid()
 
 ax.legend(fontsize=30)
@@ -38,7 +61,7 @@ axes1,colorbar1 = plot_by_id(25,axes=ax, label='Coax 6')
 ax.set_xlabel('Frequency (GHz)',size=30)
 ax.set_ylabel('S11 (dB)',size=30)
 ax.tick_params(labelsize=20) 
-ax.set_title('RT reflection test',size=30)
+ax.set_title('RT reflection test - FB 25 May 2021 - database.db',size=30)
 ax.grid()
 
 ax.legend(fontsize=30)
